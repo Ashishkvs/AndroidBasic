8b14d5d6d91e332c74f644cf7d831eb1f9afb84e
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2021-10-14 18:13:55 +0530

    added toggle button

--- app/src/main/java/com/imagegrafia/androidbasic/MaterialLookActivity.java
+++ app/src/main/java/com/imagegrafia/androidbasic/MaterialLookActivity.java
@@ -3,12 +3,24 @@ package com.imagegrafia.androidbasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
 public class MaterialLookActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_look);
+        MaterialButtonToggleGroup toggleButtonGroup = findViewById(R.id.toggleGroupButton);
+        toggleButtonGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+            Log.i(TAG,"checkId"+checkedId);
+        });
     }
 }
