2c8cb225ab81ba52e6903edab9405692119dad28
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2021-10-14 17:43:42 +0530

    material button added

---
+++ app/src/main/java/com/imagegrafia/androidbasic/MaterialLookActivity.java
@@ -0,0 +1,14 @@
+package com.imagegrafia.androidbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaterialLookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_look);
+    }
+}
