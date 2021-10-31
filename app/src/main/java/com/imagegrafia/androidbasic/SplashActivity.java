0c2b4b700c0856a5fc0d28fb015df4c1221014f8
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2021-10-31 16:37:52 +0530

    added logo file

--- app/src/main/java/com/imagegrafia/androidbasic/SplashActivity.java
+++ app/src/main/java/com/imagegrafia/androidbasic/SplashActivity.java
@@ -1,14 +1,44 @@
 package com.imagegrafia.androidbasic;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SplashActivity extends AppCompatActivity {
+
+
+    long animTime = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView iv_logo_splash = findViewById(R.id.iv_logo_splash);
+        TextView logo_txt = findViewById(R.id.logo_txt);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(iv_logo_splash, "y", 400f);
+        ObjectAnimator animatorName = ObjectAnimator.ofFloat(logo_txt, "x", 400f);
+
+        animatorY.setDuration(animTime);
+        animatorName.setDuration(animTime);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animatorY, animatorName);
+        animatorSet.start();
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
